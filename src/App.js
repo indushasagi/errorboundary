600be5cc0b3b89c50d81e8dc3687246623ce20d9
indushasagi <indusha.sagi@gmail.com>
2021-10-20 20:49:38 +0530

    converted function component to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import ErrorBoundary from './ErrorBoundaries';
 import './App.css';
 import Button from './Button';
+import React from 'react';
 
-function App() {
-  return (
-    <ErrorBoundary>
-      <div className="App">
-          <Button />
-      </div>
-    </ErrorBoundary>
-  );
+class App extends React.Component {
+  constructor(props){
+      super(props);
+  }
+  render(){
+    return (
+      <ErrorBoundary>
+        <div className="App">
+            <Button />
+        </div>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,19 @@
-const Button = () => {
-    const buttonClick = () => {
+import React from "react";
+
+class Button extends React.Component{
+    constructor(props){
+        super(props);
+        this.buttonClick = this.buttonClick.bind(this);
+    }
+    buttonClick () {
         alert('test');
         throw new Error('Error boundary test')
     }
-    return(
-        <input type="button" value="Click" onClick={buttonClick}/>
-    )
+    render() {
+        return(
+            <input type="button" value="Click" onClick={this.buttonClick}/>
+        )
+    }
 }
 
 export default Button;
