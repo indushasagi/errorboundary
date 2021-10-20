229c3fd09f7e7d10f71e9ec9223eb2694245626d
indushasagi <indusha.sagi@gmail.com>
2021-10-20 21:03:16 +0530

    added counter

--- src/Button.js
+++ src/Button.js
@@ -4,14 +4,22 @@ class Button extends React.Component{
     constructor(props){
         super(props);
         this.buttonClick = this.buttonClick.bind(this);
+        this.state={
+            counter: 0
+        }
     }
     buttonClick () {
-        alert('test');
-        throw new Error('Error boundary test')
+        if(counter === 3){
+            throw new Error('Error boundary test')
+        }
+       this.setState({counter: counter+1});
     }
     render() {
         return(
+            <>
+            <p>{this.state.counter}</p>
             <input type="button" value="Click" onClick={this.buttonClick}/>
+            </>
         )
     }
 }
